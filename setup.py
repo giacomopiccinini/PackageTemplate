6af086380266f727cc44981fea2dad8355c83973
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-09-12 13:15:14 +0200

    [ADD] Add template for setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='<name of the package>',
+    version='0.1.0',    
+    description='<A description of your package>',
+    url='https://github.com/giacomopiccinini/<name of the repo>',
+    author='Giacomo Piccinini',
+    author_email='giacomo@sparkd.ai',
+    license='MIT',
+    packages=['<name of the package>'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
